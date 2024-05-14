7bc0604f91829f20dffff1d219af2944a6391406
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-05-14 15:50:33 +0300

    markup created

---
+++ src/main.js
@@ -0,0 +1,10 @@
+//import SimpleLightbox from "simplelightbox";
+//import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+
+//const lightbox = new SimpleLightbox('.gallery a', {
+//  captionsData: "alt",
+//  captionsDelay: 250
+//});
