fd7bcbcd62d2306878f7dc9b28f4e3acc2865fce
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-05-16 13:24:38 +0000

    Deploying to gh-pages from @ olhaHrb/goit-js-hw-11@477ad7d1aa5826828c9f4d7c9cda3a4b0833d9fd 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,25 +1,2 @@
-import{S as p,i as m}from"./assets/vendor-0fc460d7.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const h="https://pixabay.com/api/",d="43873427-52524e51f424820d3ed845203",f=`?key=${d}&image_type=photo&orientation=horizontal&safesearch=true`,y=new p(".gallery a",{captionsData:"alt",captionsDelay:250});function g(s){return fetch(`${h}${f}&q=${s}`).then(r=>{if(!r.ok)throw new Error("Sorry, there are no images matching your search query. Please try again!");return r.json()})}const L=document.querySelector(".gallery"),$=document.querySelector(".form"),n=document.querySelector(".loader");$.addEventListener("submit",S);function S(s){s.preventDefault(),n.classList.remove("is-hidden");const r=s.currentTarget,i=r.elements.searchInput.value;!i==""?g(i).then(a=>{E(a),y.refresh()}).catch(a=>{v(a)}).finally(()=>{r.reset(),n.classList.add("is-hidden")}):n.classList.add("is-hidden")}function v(s){alert(s)}function E(s){const r=s.hits;if(r.length===0)m.error({message:"Sorry, there are no images matching your search query. Please try again!"});else{const i=r.map(({webformatURL:a,largeImageURL:e,tags:t,likes:o,views:c,comments:l,downloads:u})=>`
-        <li class="gallery-item">
-            <a class="gallery-link" href=${e}>
-                <img class="gallery-image" src=${a} alt=${t} />
-            </a>
-            <ul class="gallery-caption">
-              <li class="caption-item">
-                <h3 class="caption-title">Likes</h3>
-                <p class="caption-value">${o}</p>
-              </li>
-              <li class="caption-item">
-                <h3 class="caption-title">Views</h3>
-                <p class="caption-value">${c}</p>
-              </li>
-              <li class="caption-item">
-                <h3 class="caption-title">Comments</h3>
-                <p class="caption-value">${l}</p>
-              </li>
-              <li class="caption-item">
-                <h3 class="caption-title">Downloads</h3>
-                <p class="caption-value">${u}</p>
-              </li>
-            </ul>
-        </li>`).join("");L.innerHTML=i}}
+import{S as c}from"./assets/vendor-338edac8.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();function a(o){return fetch(`${BASE_URL}${params}&q=${o}`).then(t=>{if(!t.ok)throw new Error;return t.json()})}function l(o){o.preventDefault(),loader.classList.remove("is-hidden");const t=o.currentTarget,s=t.elements.searchInput.value;!s==""?a(s).then(n=>{renderImageCard(n),lightbox.refresh()}).catch(n=>{d(n)}).finally(()=>{t.reset(),loader.classList.add("is-hidden")}):loader.classList.add("is-hidden")}function d(o){alert(o)}new c(".gallery a",{captionsData:"alt",captionsDelay:250});document.querySelector(".gallery");const u=document.querySelector(".form");document.querySelector(".loader");u.addEventListener("submit",l);
 //# sourceMappingURL=commonHelpers.js.map
